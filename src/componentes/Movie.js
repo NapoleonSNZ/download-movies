e94cb21a0ec520e012b7952c77d8ca852643be85
Napoleón Sáenz <napoleonsaenz85@gmail.com>
2022-05-10 00:08:09 -0600

    createContenedor
    Movie contenedeor

--- src/componentes/Movie.js
+++ src/componentes/Movie.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React from 'react';
 
-function Movie(){
+export function Movie(){
   return(
     <div className='contenedor-movie'>
       <img 
         className='imagen-movie' 
         src={require('../img/movie-sonic.jpg')}
         alt='SonicMovie'/>
+      <div className='contenedor-texto-movie'>
+        <p className='nombre-movie'>Sonic 2: La Película / Sonic the Hedgehog 2</p>
+        <p className='elenco-movie'>Jim Carey</p>
+        <p className='genero-movie'>Accion, Comedia</p>
+        <p className='texto-movie'>Después de establecerse en Green Hills, Sonic se muere por
+         demostrar que tiene madera de auténtico héroe, pero Robotnik regresa con nuevo
+          compañero Knuckles, en busca de una esmeralda que tiene el poder de destruir 
+          civilizaciones, pero Sonic no esta solo lo ayudara Tails.
+        </p>
+      </div>
     </div>
   );  
 }
+
+//exportacion Nombrada o por Defecto
+    //exportación por defecto
+export default Movie;
