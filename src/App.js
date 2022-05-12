f8f8abe5a21457de637058c56b51613d7045e22b
Napoleón Sáenz <napoleonsaenz85@gmail.com>
2022-05-12 17:38:42 -0600

    othersComponents

--- src/App.js
+++ src/App.js
@@ -6,10 +6,32 @@ function App() {
   return (
     <div className="App">
       <div className='contenedor-principal'>
-      <h1>ESTRENOS 2022</h1>
-      <br></br>
-      <h4>Descarga los Estrenos mas recientes ♥' </h4>
-      <Movie />
+      <h1>DESCARGAS ESTRENOS 2022 </h1>
+      <Movie 
+        nombre='Sonic 2: La Película'
+        ano='2022'
+        actor='Jim Carey'
+        imagen='sonic'
+        link='https://www.mediafire.com/file/eux0ocapmxtd73a/STH2720.zip'
+        descripcion='Después de establecerse en Green Hills, Sonic se muere por demostrar que tiene madera de auténtico héroe, pero Robotnik regresa con nuevo compañero Knuckles, en busca de una esmeralda que tiene el poder de destruir civilizaciones, pero Sonic no esta solo lo ayudara Tails.'
+      />
+      <Movie 
+        nombre='The Batman'
+        ano='2022'
+        actor=' Robert Pattinson, Zoë Kravitz'
+        imagen='batman'
+        link='https://www.mediafire.com/file/j1r0j41pplv3pms/g4t0nbwn2k22.exe'
+        descripcion='
+        Cuando un asesino se dirige a la élite de Gotham con una serie de maquinaciones sádicas, un rastro envía Batman a una investigación. A medida que las pruebas comienzan a acercarse a su casa, Batman debe desenmascarar al culpable y hacer justicia a la corrupción en Gotham City.'
+      />
+      <Movie 
+        nombre='Metal Lords'
+        ano='2022'
+        actor='Jaeden Martell, Adrian Greensmith, Isis Hainsworth'
+        imagen='metal'
+        link='https://www.mediafire.com/file/tehw6hn7daemxyb/g4t0mtlrds.rar'
+        descripcion='Metal Lords 2022 Mega. Para los inadaptados adolescentes Hunter y Kevin, el camino a la gloria es claro: dedicarse al metal. Gana la Batalla de las Bandas. y ser adorados como dioses.'
+      />
       </div>
     </div>
   );
--- src/componentes/Movie.js
+++ src/componentes/Movie.js
@@ -1,21 +1,18 @@
 import React from 'react';
+import '../stylesheets/Movie.css';
 
-export function Movie(){
+ function Movie(props){
   return(
     <div className='contenedor-movie'>
       <img 
         className='imagen-movie' 
-        src={require('../img/movie-sonic.jpg')}
+        src={require(`../img/movie-${props.imagen}.jpg`)}
         alt='SonicMovie'/>
       <div className='contenedor-texto-movie'>
-        <p className='nombre-movie'>Sonic 2: La Película / Sonic the Hedgehog 2</p>
-        <p className='elenco-movie'>Jim Carey</p>
-        <p className='genero-movie'>Accion, Comedia</p>
-        <p className='texto-movie'>Después de establecerse en Green Hills, Sonic se muere por
-         demostrar que tiene madera de auténtico héroe, pero Robotnik regresa con nuevo
-          compañero Knuckles, en busca de una esmeralda que tiene el poder de destruir 
-          civilizaciones, pero Sonic no esta solo lo ayudara Tails.
-        </p>
+        <p className='nombre-movie'>{props.nombre} {props.ano}</p>
+        <p className='elenco-movie'>{props.actor}</p>
+        <a href={`${props.link}`} rel="noreferrer" target={'_blank' } className='link-movie'>DESCARGA AQUI!!!</a>
+        <p className='texto-movie'>{props.descripcion} </p>
       </div>
     </div>
   );  
